a324e6a2c5ef2f4432faff5d40cf6f53d3626d4b
JCMedinaG <42121290+JCMedinaG@users.noreply.github.com>
2018-10-01 19:00:25 -0400

    Delete practice4.py

--- PyBank/practice4.py
+++
@@ -1,6 +0,0 @@
-user_play= "y"
-while user_play== "y":
-    user_number= input("How many numbers? ")
-    for x in range(int(user_number)):
-      print(x)
-user_play= input("Continue: (y)es or (n)o? ")
